9617751578e2a733cf079e45c38c3d672acfc8a8
Quentin Rousseau <contact@quent.in>
2022-03-22 22:41:28 -0700

    Support new scopes

--- lib/omniauth/strategies/confluence_oauth2.rb
+++ lib/omniauth/strategies/confluence_oauth2.rb
@@ -4,7 +4,7 @@ require 'omniauth/strategies/oauth2'
 require 'uri'
 
 # Potential scopes: https://developer.atlassian.com/cloud/jira/platform/scopes/
-# offline_access read:confluence-space.summary read:confluence-props read:confluence-content.all read:confluence-content.summary search:confluence
+# offline_access read:user:confluence read:content-details:confluence read:content.metadata:confluence write:content:confluence
 #
 # Separate scopes with a space (%20)
 # https://developer.atlassian.com/cloud/confluence/oauth-2-authorization-code-grants-3lo-for-apps/
@@ -15,13 +15,15 @@ module OmniAuth
     class ConfluenceOauth2 < OmniAuth::Strategies::OAuth2
       option :name, 'confluence_oauth2'
       option :client_options,
-             site: 'https://auth.atlassian.com',
+             site: 'https://api.atlassian.com',
              authorize_url: 'https://auth.atlassian.com/authorize',
              token_url: 'https://auth.atlassian.com/oauth/token',
              audience: 'api.atlassian.com'
       option :authorize_params,
              prompt: 'consent',
              audience: 'api.atlassian.com'
+      
+      option :new_scopes, false
 
       uid do
         raw_info['myself']['account_id']
@@ -46,12 +48,17 @@ module OmniAuth
       def raw_info
         return @raw_info if @raw_info
 
-        # NOTE: api.atlassian.com, not auth.atlassian.com!
-        accessible_resources_url = 'https://api.atlassian.com/oauth/token/accessible-resources'
-        sites = JSON.parse(access_token.get(accessible_resources_url).body)
+        if options.new_scopes
+          access_token.options[:mode] = :header
+          myself ||= access_token.get('wiki/rest/api/user/current', :headers => { 'Content-Type' => 'application/json' }).parsed
+        else
+          # NOTE: api.atlassian.com, not auth.atlassian.com!
+          accessible_resources_url = 'https://api.atlassian.com/oauth/token/accessible-resources'
+          sites = JSON.parse(access_token.get(accessible_resources_url).body)
 
-        myself_url = "https://api.atlassian.com/me"
-        myself = JSON.parse(access_token.get(myself_url).body)
+          myself_url = "https://api.atlassian.com/me"
+          myself = JSON.parse(access_token.get(myself_url).body)
+        end
 
         @raw_info ||= {
           'sites' => sites,
